31fca26fe893c32c442cfd4921cc2da60d5460ce
Samidha Dhawale <samidhadd21@gmail.com>
2026-04-01 03:12:26 +0530

    Added more fonts and changed positioning

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div className="fixed w-full top-4 flex justify-center z-50 ">
-      <div className="flex justify-center gap-6 px-8 py-3 bg-black text-white rounded-full border-white border-2">
+      <div className="flex justify-center gap-6 px-6 py-2 text-white rounded-full bg-white/10 backdrop-blur-md border border-white/20">
         <Link to="/" className="bg-purple-500 text-white py-2 px-4 rounded-3xl">Home</Link>
         <Link to="/events" className="hover:bg-purple-400 text-white py-2 px-4 rounded-3xl">Events</Link>
         <Link to="/registration" className="hover:bg-purple-400 text-white py-2 px-4 rounded-3xl">Registration</Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,27 @@
 import PhotoCard from "../components/PhotoCard";
-import astro from "../assets/astronaut.jpeg";
+import astro from "../assets/astronaut1.png";
 
 function Home() {
   return (
-    <div className="bg-[#011121] min-h-screen text-white">
-
+    <div className="bg-[#310055] min-h-screen text-white">
       <div className="flex items-center justify-between px-10 py-20">
-
         <div>
+          <div className="animate-float">
+            <img src={astro} className="h-[300px] animate-slideIn rotate-4" />
+          </div>
 
-          <h1 className="text-5xl font-bold flex">
+          <div className="flex gap-4 mt-6">
+            <button className="text-[#dabfff] font-roboto px-4 py-2 transition bg-white/10 backdrop-blur-md border border-white/20 rounded-xl hover:bg-white/5">
+              Register now
+            </button>
+            <button className="text-[#dabfff] font-roboto px-4 py-2 transition bg-white/10 backdrop-blur-md border border-white/20 rounded-xl hover:bg-white/5">
+              Explore events
+            </button>
+          </div>
+        </div>
+
+        <div className="text-right">
+          <h1 className="text-8xl font-bold flex justify-end font-[Bebas_Neue] tracking-wider">
             {"Euphoria".split("").map((letter, index) => (
               <span
                 key={index}
@@ -20,29 +32,11 @@ function Home() {
               </span>
             ))}
           </h1>
-
-          <div className="flex gap-4 mt-6">
-            <button className="bg-blue-500 px-4 py-2 rounded-md hover:bg-blue-600 transition">
-              Register now
-            </button>
-            <button className="bg-blue-500 px-4 py-2 rounded-md hover:bg-blue-600 transition">
-              Explore events
-            </button>
-          </div>
-
+          <p className="text-3xl mt-4 text-right font-[Dancing_Script] text-[#f54952]">Lose yourself in the magic of Euphoria!</p>
         </div>
-
-        <div>
-          <img
-            src={astro}
-            className="h-[300px] animate-astro"
-          />
-        </div>
-
       </div>
 
       <PhotoCard />
-
     </div>
   );
 }
