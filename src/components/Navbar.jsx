58f060391d29c39d191a5f52748f1117a6dc40cd
Samidha Dhawale <samidhadd21@gmail.com>
2026-03-31 19:23:11 +0530

    Navbar and images added to codebase

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,16 @@
-function Navbar(){
-    
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div className="fixed w-full top-4 flex justify-center z-50 ">
+      <div className="flex justify-center gap-6 px-8 py-3 bg-black text-white rounded-4xl border-red-500 border-2">
+        <Link to="/" className="bg-purple-500 text-white py-2 px-4 rounded-3xl">Home</Link>
+        <Link to="/about" className="hover:bg-purple-400 text-white py-2 px-4 rounded-3xl">About Us</Link>
+        <Link to="/gallery" className="hover:bg-purple-400 text-white py-2 px-4 rounded-3xl">Gallery</Link>
+        <Link to="/contact" className="hover:bg-purple-400 text-white py-2 px-4 rounded-3xl">Contact</Link>
+      </div>
+    </div>
+  );
 }
+
+export default Navbar;
---
+++ src/components/PhotoCard.jsx
@@ -0,0 +1,14 @@
+function PhotoCard(){
+    return(
+        <div>
+            <img src="/img1.jpg" className="h-40 rounded-lg" />
+            <img src="/img1.jpg" className="h-40 rounded-lg" />
+            <img src="/img1.jpg" className="h-40 rounded-lg" />
+            <img src="/img1.jpg" className="h-40 rounded-lg" />
+            <img src="/img1.jpg" className="h-40 rounded-lg" />
+            <img src="/img1.jpg" className="h-40 rounded-lg" />
+            <img src="/img1.jpg" className="h-40 rounded-lg" />
+            <img src="/img1.jpg" className="h-40 rounded-lg" />
+        </div>
+    )
+}
