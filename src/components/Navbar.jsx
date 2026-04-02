afefe2295130726eae7d01bf2722f5a329612a08
Samidha Dhawale <samidhadd21@gmail.com>
2026-04-03 00:07:58 +0530

    Thats it for today

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div className="fixed w-full top-4 flex justify-center z-50 ">
-      <div className="flex justify-center gap-6 px-6 py-2 rounded-full bg-white/10 backdrop-blur-md border border-white/20">
+      <div className="flex justify-center gap-6 px-6 py-2 rounded-full bg-black/40 backdrop-blur-md border border-white/20">
         <Link to="/" className="bg-[#F5E6C8] text-[#c05754] font-[Almendra] text-base py-2 px-4 rounded-3xl">Home</Link>
         <Link to="/events" className="hover:bg-[#F5E6C8]/80 text-[#c05754] font-[Almendra] text-base py-2 px-4 rounded-3xl">Events</Link>
         <Link to="/registration" className="hover:bg-[#F5E6C8]/80 text-[#c05754] font-[Almendra] text-base py-2 px-4 rounded-3xl">Registration</Link>
