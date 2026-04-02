30cc717be47ad7199b204cc3371ef71dc66a8f3c
Samidha Dhawale <samidhadd21@gmail.com>
2026-04-02 23:31:22 +0530

    Added fonts and styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,11 @@ function Navbar() {
   return (
     <div className="fixed w-full top-4 flex justify-center z-50 ">
       <div className="flex justify-center gap-6 px-6 py-2 rounded-full bg-white/10 backdrop-blur-md border border-white/20">
-        <Link to="/" className="bg-[#F5E6C8] text-[#c05754] py-2 px-4 rounded-3xl">Home</Link>
-        <Link to="/events" className="hover:bg-[#F5E6C8]/80 text-[#c05754] py-2 px-4 rounded-3xl">Events</Link>
-        <Link to="/registration" className="hover:bg-[#F5E6C8]/80 text-[#c05754] py-2 px-4 rounded-3xl">Registration</Link>
-        <Link to="/gallery" className="hover:bg-[#F5E6C8]/80 text-[#c05754] py-2 px-4 rounded-3xl">Gallery</Link>
-        <Link to="/contact" className="hover:bg-[#F5E6C8]/80 text-[#c05754] py-2 px-4 rounded-3xl">Contact Us</Link>
+        <Link to="/" className="bg-[#F5E6C8] text-[#c05754] font-[Almendra] text-base py-2 px-4 rounded-3xl">Home</Link>
+        <Link to="/events" className="hover:bg-[#F5E6C8]/80 text-[#c05754] font-[Almendra] text-base py-2 px-4 rounded-3xl">Events</Link>
+        <Link to="/registration" className="hover:bg-[#F5E6C8]/80 text-[#c05754] font-[Almendra] text-base py-2 px-4 rounded-3xl">Registration</Link>
+        <Link to="/gallery" className="hover:bg-[#F5E6C8]/80 text-[#c05754] font-[Almendra] text-base py-2 px-4 rounded-3xl">Gallery</Link>
+        <Link to="/contact" className="hover:bg-[#F5E6C8]/80 text-[#c05754] font-[Almendra] text-base py-2 px-4 rounded-3xl">Contact Us</Link>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import PhotoCard from "../components/PhotoCard";
 import DiscoScene from "../components/DiscoScene";
+import cta from "../assets/cta.png";
 
 function Home() {
   return (
     <div className="relative bg-[#1C0F0F] min-h-screen text-white">
       <div className="flex items-center justify-between px-10 pt-10">
-        <DiscoScene className="-top-10 relative"/>
+        <DiscoScene className="-top-10 relative" />
 
         <div className="text-right">
           <h1 className="text-8xl font-bold flex justify-end font-[Bebas_Neue] tracking-wider text-[#F5E6C8]">
@@ -20,8 +21,26 @@ function Home() {
             ))}
           </h1>
           <p className="text-3xl text-right font-[Dancing_Script] text-[#c05754]">
-            Lose yourself in the magic of Euphoria!
+            {"Where every event feels iconic, every plan is effortless, and every memory is worth the hype!!!"
+              .split("")
+              .map((word, index) => (
+                <span
+                  key={index}
+                  className="opacity-0 animate-letter"
+                  style={{ animationDelay: `${index * 0.01}s` }}
+                >
+                  {word}
+                </span>
+              ))}
           </p>
+          <div className="flex justify-end">
+            <div className="relative mt-6 inline-block cursor-pointer group">
+              <img src={cta} className="w-50" />
+              <span className="absolute inset-0 flex items-center justify-end mr-4 text-lg font-bold text-black tracking-wide group-hover:scale-105 transition font-[Almendra]">
+                Register Now
+              </span>
+            </div>
+          </div>
         </div>
       </div>
 
